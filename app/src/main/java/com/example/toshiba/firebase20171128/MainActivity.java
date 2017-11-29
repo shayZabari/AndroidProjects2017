d19539b6c32425cb7be6ba0d44f724c6eff2c449
shayZabari <6650000@gmail.com>
2017-11-29 18:19:17 +0200

    good

--- app/src/main/java/com/example/toshiba/firebase20171128/MainActivity.java
+++ app/src/main/java/com/example/toshiba/firebase20171128/MainActivity.java
@@ -11,25 +11,41 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-Button btnFire;
-EditText etUniversityName;
+    Button btnFire;
+    EditText etUniversityName, etUniversityUrl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnFire = findViewById(R.id.btnFire);
         etUniversityName = findViewById(R.id.etUniversityName);
+        etUniversityUrl = findViewById(R.id.etUniversityUrl);
         // Write a message to the database
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message1").child("shay");
 
-        myRef.setValue("Hello, World123123!");
+        final FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference mRootReference = firebaseDatabase.getReference();
+        final DatabaseReference mUniversities = mRootReference.child("universities");
+//        final DatabaseReference mUniversityColor = mRootReference.child("color");
+
+
+//        myRef.setValue("Hello, World123123!");
         btnFire.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String universityName = etUniversityName.getText().toString();
-                DatabaseReference universities = database.getReference(universityName);
-                universities.setValue("urltest");
+                String url = etUniversityUrl.getText().toString();
+                final DatabaseReference mUniversityName = mUniversities.child(universityName);
+                final DatabaseReference mUniversityUrl = mUniversityName.child("url");
+//                mUniversityName.setValue("test");
+                mUniversityUrl.setValue(url);
+
+//                mUniversityName.setValue(universityName);
+//
+//                DatabaseReference universities = mRef.getReference("Universities");
+//                DatabaseReference urlDb = universities.child("url");
+//                DatabaseReference logoDb = universities.child("logo");
+//                universities.(universityName);
             }
         });
     }
